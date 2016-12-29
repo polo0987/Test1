a5cfa6c0f2322e2e3a45bc54b24bc72fbd0e9b44
polo0987 <polo987@163.com>
2016-12-29 11:02:04 +0800

    Add files via upload

---
+++ batch.sql
@@ -0,0 +1,145 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 172.16.2.60thread
+Source Server Version : 50620
+Source Host           : 172.16.2.54:3309
+Source Database       : thread
+
+Target Server Type    : MYSQL
+Target Server Version : 50620
+File Encoding         : 65001
+
+Date: 2016-11-02 15:16:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for batch_job_execution
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_execution`;
+CREATE TABLE `batch_job_execution` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) DEFAULT NULL,
+  `JOB_INSTANCE_ID` bigint(20) DEFAULT NULL,
+  `CREATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `START_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `END_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `STATUS` varchar(10) DEFAULT NULL,
+  `EXIT_CODE` varchar(20) DEFAULT NULL,
+  `EXIT_MESSAGE` varchar(2500) DEFAULT NULL,
+  `LAST_UPDATED` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `JOB_CONFIGURATION_LOCATION` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`JOB_EXECUTION_ID`),
+  KEY `JOB_INSTANCE_EXECUTION_FK` (`JOB_INSTANCE_ID`),
+  CONSTRAINT `JOB_INSTANCE_EXECUTION_FK` FOREIGN KEY (`JOB_INSTANCE_ID`) REFERENCES `batch_job_instance` (`JOB_INSTANCE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_job_execution_context
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_execution_context`;
+CREATE TABLE `batch_job_execution_context` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `SHORT_CONTEXT` varchar(2500) NOT NULL,
+  `SERIALIZED_CONTEXT` text,
+  PRIMARY KEY (`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXEC_CTX_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_job_execution_params
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_execution_params`;
+CREATE TABLE `batch_job_execution_params` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `TYPE_CD` varchar(6) NOT NULL,
+  `KEY_NAME` varchar(100) NOT NULL,
+  `STRING_VAL` varchar(250) DEFAULT NULL,
+  `DATE_VAL` datetime DEFAULT NULL,
+  `LONG_VAL` bigint(20) DEFAULT NULL,
+  `DOUBLE_VAL` double DEFAULT NULL,
+  `IDENTIFYING` char(1) NOT NULL,
+  KEY `JOB_EXEC_PARAMS_FK` (`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXEC_PARAMS_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_job_execution_seq
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_execution_seq`;
+CREATE TABLE `batch_job_execution_seq` (
+  `ID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_job_instance
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_instance`;
+CREATE TABLE `batch_job_instance` (
+  `JOB_INSTANCE_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) DEFAULT NULL,
+  `JOB_NAME` varchar(100) NOT NULL,
+  `JOB_KEY` varchar(2500) DEFAULT NULL,
+  PRIMARY KEY (`JOB_INSTANCE_ID`),
+  KEY `idx_job_instance_name_key` (`JOB_NAME`,`JOB_KEY`(255))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_job_seq
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_job_seq`;
+CREATE TABLE `batch_job_seq` (
+  `ID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_step_execution
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_step_execution`;
+CREATE TABLE `batch_step_execution` (
+  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) NOT NULL,
+  `STEP_NAME` varchar(100) DEFAULT NULL,
+  `JOB_EXECUTION_ID` bigint(20) DEFAULT NULL,
+  `START_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `END_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `STATUS` varchar(10) DEFAULT NULL,
+  `COMMIT_COUNT` bigint(20) DEFAULT NULL,
+  `READ_COUNT` bigint(20) DEFAULT NULL,
+  `FILTER_COUNT` bigint(20) DEFAULT NULL,
+  `WRITE_COUNT` bigint(20) DEFAULT NULL,
+  `READ_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `WRITE_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `PROCESS_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `ROLLBACK_COUNT` bigint(20) DEFAULT NULL,
+  `EXIT_CODE` varchar(20) DEFAULT NULL,
+  `EXIT_MESSAGE` longtext,
+  `LAST_UPDATED` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`STEP_EXECUTION_ID`),
+  KEY `JOB_EXECUTION_STEP_FK` (`JOB_EXECUTION_ID`),
+  KEY `idx_job_exec_step_version` (`VERSION`),
+  KEY `idx_job_exec_step_name_job_exec_id` (`STEP_NAME`,`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXECUTION_STEP_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_step_execution_context
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_step_execution_context`;
+CREATE TABLE `batch_step_execution_context` (
+  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
+  `SHORT_CONTEXT` varchar(2500) NOT NULL,
+  `SERIALIZED_CONTEXT` text,
+  PRIMARY KEY (`STEP_EXECUTION_ID`),
+  CONSTRAINT `STEP_EXEC_CTX_FK` FOREIGN KEY (`STEP_EXECUTION_ID`) REFERENCES `batch_step_execution` (`STEP_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for batch_step_execution_seq
+-- ----------------------------
+DROP TABLE IF EXISTS `batch_step_execution_seq`;
+CREATE TABLE `batch_step_execution_seq` (
+  `ID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
